ebd562330ea8a413dd72841df4d7c63c32b8d95a
Prince-Hamza <princehamzi.mine@gmail.com>
2023-02-17 09:58:25 +0500

    add axios

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { viteCommonjs, esbuildCommonjs } from '@originjs/vite-plugin-commonjs'
 
 export default defineConfig({
+ 
   plugins: [viteCommonjs(), react()],
   build: {
     outDir: 'build',
